c251ab0062484e1e1f32fe54d63067d4930e5199
Piyush Banarjee <145681750+piyushisdrinkingwater@users.noreply.github.com>
2026-02-02 18:49:46 +0530

    Preprocessing update

--- Dataset/preprocessing.py
+++ Dataset/preprocessing.py
@@ -7,13 +7,31 @@ df["Emission_Intensity"] = (
 )
 df = df.sort_values(["Company_ID", "Date"])
 
+# One hot encoding
 df = pd.get_dummies(
     df,
-    columns=["Fuel_Type", "Industry_Type"],
+    columns=["Fuel_Type", "Industry_Type", "Optimization_Scenario"],
     drop_first= True
 )
 df[df.select_dtypes(include="bool").columns] = (
     df.select_dtypes(include="bool").astype(int)
 )
 
-df.to_csv("Features.csv", index=False)
+# Binary Encoding
+df["Transaction_Type"] = df["Transaction_Type"].map({"Sell": 1, "Buy": 0}) 
+df["Verification_Status"] = df["Verification_Status"].map({"Disputed": 0, "Verified": 1}) 
+
+# Making dates more useful
+df["Date"] = pd.to_datetime(df["Date"])
+
+df["year"] = df["Date"].dt.year
+df["month"] = df["Date"].dt.month
+df["day"] = df["Date"].dt.day
+
+df.drop(columns=["Date"], inplace=True)
+#  making company ids more useful
+freq = df["Company_ID"].value_counts()
+df["company_freq"] = df["Company_ID"].map(freq)
+df.drop(columns=["Company_ID"], inplace=True)
+
+df.to_csv("Dataset\Features2.csv", index=False)
